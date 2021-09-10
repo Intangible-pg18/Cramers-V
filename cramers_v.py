ef5205d66d36e2d4ae32c78391f794f0d62b3410
Prateek Shukla <53486253+Intangible-pg18@users.noreply.github.com>
2021-09-10 09:27:59 +0530

    Create cramers_v.py

---
+++ cramers_v.py
@@ -0,0 +1,19 @@
+#cramer's v
+import numpy as np
+import pandas as pd
+import scipy.stats as ss
+
+def cramers_v(x,y):
+    contingency_table = pd.crosstab(df[x], df[y])
+    if contingency_table.shape[0]==2:
+        correct=False
+    else:
+        correct=True
+    chi2 = ss.chi2_contingency(contingency_table)[0]
+    n = contingency_table.sum().sum()
+    phi2_coff = chi2/n
+    r,c = contingency_table.shape
+    unbiased_phi2_coff = max(0, (phi2_coff - (((c-1)*(r-1))/(n-1))))    
+    r_corr = r - (((r-1)**2)/(n-1))
+    c_corr = c - (((c-1)**2)/(n-1))
+    return np.sqrt(unbiased_phi2_coff / min((c_corr-1), (r_corr-1)))
